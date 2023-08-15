2fd309f10ea6969ce67077d5f9c56f03ca004638
Jonathan Diaz <jonathan.dimor@hotmail.com>
2023-08-15 14:18:42 -0600

    Agregar archivos de proyecto.

---
+++ ssepnominas/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+namespace ssepnominas
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            AppCenter.Start("android={f75ef0be-6a13-4bda-bd10-05a0a3bbfb01};" +
+                  "ios={79267cdd-47a5-4bdb-ab54-22d251dc208e};",
+                  typeof(Analytics), typeof(Crashes));
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
